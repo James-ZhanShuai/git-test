afaae3725370e1674610d1c135207ba581daa7b1
james <2274346723@qq.com>
2019-04-09 21:25:18 +0800

    username and password

--- git-test-project/src/com/ithiema/domian/testGit.java
+++ git-test-project/src/com/ithiema/domian/testGit.java
@@ -3,7 +3,8 @@ package com.ithiema.domian;
 public class testGit {
     public static void main(String[] args) {
         System.out.println("hello git");
-
         System.out.println("hello git");
+        System.out.println("username:James-ZhanShuai");
+        System.out.println("password:zs13550895684");
     }
 }
